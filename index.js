37969efd78152d279f484e1bdf0be7f92c0c088a
notTGY <microcraft47@gmail.com>
2021-06-30 00:55:38 +0300

    THIS IS MC PE???

--- index.js
+++ index.js
@@ -1,62 +1,70 @@
-M=[
-  [0,0,7],
-  [1,1,3],
-  [1,0,3],
-  [1,-1,3],
-  [-1,0,2],
-  [0,2,3],
-  [-1,1,3],
-  [-1,-1,3],
-  [0,-1,3],
-  [2,0,6],
-  [2,0,5],
-  [2,0,4],
-  [2,0,3],
-  [2,0,2],
-  [2,0,1],
-  [2,0,0],
-]
-
-K=X=Y=Z=A=B=T=0,x=c.getContext`2d`
-S=e=>Math.sin(e)
-C=e=>Math.cos(e)
-F=e=>2*Math.asin(1-e/250)
-G=e=>new Date().getTime();
-(U=e=>{
-  E=G()
-  c.width=c.height=40
-  for(N=1600;N--;){
-    M.map(O=>{
-      for(D=1;D<7;D+=.2) {
-        px = (N%40)/20-1
-        py = N/800-1
-        dx = px*C(A) + S(A)*C(B) + py*S(A)*S(B) 
-        dz = -px*S(A) + C(A)*C(B) + py*C(A)*S(B)
-        dy = - S(B) + py*C(B)
-        rx = X+dx*D
-        ry = Y+dy*D
-        rz = Z+dz*D
-        Rx = O[0]-rx
-        Ry = O[1]-ry
-        Rz = O[2]-rz
-        if (Math.abs(Rx)<0.5&&Math.abs(Ry)<0.5&&Math.abs(Rz)<0.5) 
-          x.fillRect(N%40, N/40, .8/D, 1)
-      }
-    })
+M=[]
+for (I=255;I--;){
+  for (J=255;J--;){
+    M.push([I,2*Math.random()|0,J])
   }
-  T=(G()-E)/400
-
-  if(K--){
-    Z+=T*C(A+Math.PI*K/2)
-    X+=T*S(A+Math.PI*K/2)
-  }
-  K=0
+}
+Y=4
+X=Z=99
 
 
-  setTimeout(U,0)
-})()
+SA=SB=H=A=0,x=c.getContext`2d`
+CA=CB=1
+S=Math.sin
+C=Math.cos
+F=e=>2*Math.asin(1-e/125)
 onmousemove=e=>{
   A=-2.5*F(e.x)
   B=F(e.y)
+  CA=C(A)
+  SA=S(A)
+  CB=C(B)
+  SB=S(B)
 }
-onkeydown=e=>K={w:1,s:3,a:4,d:2}[e.key]
+onkeydown=e=>H={w:1,s:3,a:4,d:2}[e.key]
+R=(I,J,K)=>{
+  if (I<.5&&J<.5&&K<.5&&I>-.5&&J>-.5&&K>-.5) {
+    D = (255-700/++D|0).toString(16) // TODO: improve shader
+    x.fillStyle = '#'+D+D+D
+    x.fillRect(N%50, N/50, 1, 1)
+    D=8
+  }
+}
+G=e=>new Date().getTime();
+(U=e=>{
+  E=G()
+  c.width=c.height=50
+
+  V=M.filter(([I,J,K])=>{
+    cx = I-X
+    cy = J-Y
+    cz = K-Z
+    return cx**2+cy**2+cz**2<50;
+  })
+
+  for(N=2500;N--;){
+    px = (N%50)/25-1
+    py = N/1000-1
+    pp = CB + py*SB
+    dx = px*CA + SA*pp 
+    dz = CA*pp - px*SA 
+    dy = SB - py*CB
+    V.map(([I,J,K])=>{
+      for(D=1;D<7;D+=.2)
+        R(
+          I-X - dx*D,
+          J-Y - dy*D,
+          K-Z - dz*D
+        )
+    })
+  }
+
+  if(H--){
+    T=(G()-E)/400
+    Z+=T*C(A+Math.PI*H/2)
+    X+=T*S(A+Math.PI*H/2)
+  }
+
+
+  setTimeout(U,H=0)
+})()
